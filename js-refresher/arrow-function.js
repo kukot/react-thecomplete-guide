7b4fadd9b62775d8183cf4781383dc0b6aa2f288
kukot <phucnn.work@gmail.com>
2022-02-08 13:22:31 +0700

    arrow function and import export

---
+++ js-refresher/arrow-function.js
@@ -0,0 +1,11 @@
+function showName(name) {
+    alert("Your name is " + name)
+}
+
+const showMyName = (name) => {
+    console.log(name)
+}
+
+const showRandomName = () => {
+    console.log("This is random name, not a passed in argument")
+}
---
+++ js-refresher/person.js
@@ -0,0 +1,6 @@
+const person = {
+    name: 'Kevin Nguyen',
+    age: 27
+}
+
+export default person
